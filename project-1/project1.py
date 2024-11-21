6b73b335bfd78a84ec5b9d012002ffd389f0961a
Alex Smith <xelanybor@gmail.com>
2024-11-21 15:51:07 +0200

    declare methods for question 1

---
+++ project-1/project1.py
@@ -0,0 +1,26 @@
+class FrequentlyUsedWords():
+    """Reads words one at a time from a string of text and keeps track of how frequently each word is used, as well as the most frequently used words."""
+
+    wordCounts: dict[str, int] = {}
+
+    def __init__(self):
+        pass
+
+    def readWord(self, word: str):
+        """Read one word from the input string and update the corresponding word count.
+
+        Args:
+            word (str): The current word being read from the input string.
+        """
+        pass
+
+    def getFrequentlyUsedWords(self, n: int = 10) -> list[str]:
+        """Get the top n most frequently used words.
+
+        Args:
+            n (int, optional): How many most frequently words to get. Defaults to 10.
+
+        Returns:
+            list[str]: A list of the top n most frequently used words.
+        """
+        pass
