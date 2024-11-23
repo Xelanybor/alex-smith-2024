25ca09a793ffb75fbed8c0712b37a9b15637c32f
Alex Smith <xelanybor@gmail.com>
2024-11-23 15:03:25 +0200

    declare isPangram for question 3

---
+++ project-3/project3.py
@@ -0,0 +1,10 @@
+import string
+
+def isPangram(str1, alphabet=string.ascii_lowercase) -> bool:
+    """Check whether a given string is a pangram or not. A pangram is a word or sentence containing every letter of the alphabet at least once.
+
+    Args:
+        str1 (_type_): The string to check.
+        alphabet (_type_, optional): Which alphabet to use. Defaults to the default English a-z alphabet (specifically `string.ascii_lowercase`).
+    """
+    pass
