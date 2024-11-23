3a8a487a255a40ea69728512a8dde472d19231c0
Alex Smith <xelanybor@gmail.com>
2024-11-23 21:40:32 +0200

    declare numberMachine method for question 4

---
+++ project-4/project4.py
@@ -0,0 +1,15 @@
+def numberMachine(number: int) -> tuple[int, int, int]:
+    """A number machine accepts a five-digit number and transforms it in three ways, viz:
+- reverses the number. E.g. 12345 should be returned as 54321.
+- calculates the sum of its digits.
+- generates a new number by adding one to each of its digits. For example if the number that is input is 12391 then the output should be displayed as 23402.
+
+    Args:
+        number (int): The number to transform.
+
+    Returns:
+        int: The input number with digits reversed.
+        int: The sum of the input's digits.
+        int: The input number with 1 added to each of its digits.
+    """
+    pass
